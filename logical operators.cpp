280b044e0764332b523407f137c5007812175114
spandanarachamadugu7-jpg <spandanarachamadugu7@gmail.com>
2025-11-21 19:22:47 +0530

    Add files via upload

---
+++ logical operators.cpp
@@ -0,0 +1,12 @@
+//write a c program demonstrate logical operators
+#include<stdio.h>
+int main()
+{
+	int a=20,b=40,c=20,d;
+	d=(a<b)||(a=40);
+	printf("a=%d\n",a);
+	printf("b=%d\n",b);
+	printf("c=%d\n",c);
+	printf("d=%d\n",d);
+	return 0;
+}
